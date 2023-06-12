538a757d1bcad8c458daa49d3343ee7ec54151c3
Jared <jareddj34@gmail.com>
2023-06-12 16:22:28 -0700

    first commit from desktop

---
+++ app/page.jsx
@@ -0,0 +1,24 @@
+import Feed from "@components/Feed";
+
+const Home = () => {
+  return (
+    <section className="w-full flex-center
+    flex-col">
+        <h1 className = "head_text text-center">
+            Discover & Share
+            <br className="max-md:hidden"/>
+            <span className="green_gradient
+            text-center"> AI-Powered Prompts</span>
+        </h1>
+        <p className = "desc text-center">
+            Promptopia is an open-source AI prompting tool for 
+            shit you can do and stuff. Broooo keon what am i even
+            doing rn
+        </p>
+        
+      
+    </section>
+  )
+}
+
+export default Home
